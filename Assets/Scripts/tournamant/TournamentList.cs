c22c3a9e0786060aedb2c3928877b04ee09d1009
night-angel-star <niang00610@gmail.com>
2024-03-04 01:49:01 +0900

    add a function in utils

--- Assets/Scripts/tournamant/TournamentList.cs
+++ Assets/Scripts/tournamant/TournamentList.cs
@@ -16,6 +16,25 @@ public class TournamentObject
     public int max_players;
 }
 
+public class TournamentDetailObject
+{
+    public int id;
+    public string status;
+    public string name;
+    public int delay;
+    public int buy_in;
+    public int registered_players;
+    public int max_players;
+    public int count_down;
+    public int end_date;
+    public int first_blind;
+    public int prize;
+    public int rise_count;
+    public int rise_time;
+    public Dictionary<string, object> rule;
+    public int start_date;
+}
+
 public class TournamentList : MonoBehaviour
 {
     // Start is called before the first frame update
